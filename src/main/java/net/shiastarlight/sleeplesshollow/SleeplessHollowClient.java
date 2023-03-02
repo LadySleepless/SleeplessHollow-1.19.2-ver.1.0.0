5e87ead0fb5da3b0aa0abf6b7ed171115bc8c3d5
shiastarlight <shia.teague@gmail.com>
2023-03-01 19:30:55 -0800

    Hello World!

---
+++ src/main/java/net/shiastarlight/sleeplesshollow/SleeplessHollowClient.java
@@ -0,0 +1,10 @@
+package net.shiastarlight.sleeplesshollow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SleeplessHollowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
